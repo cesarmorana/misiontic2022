3cc2d44eaf78404ae2ccac8971f44c57000e4241
andrmoran <89643687+andrmoran@users.noreply.github.com>
2021-08-27 07:49:19 -0500

    version1.0
    versión 1.0

---
+++ pruebag07.java
@@ -0,0 +1,9 @@
+import javax.swing.plaf.synth.SynthScrollableTabButton;
+
+public class pruebag07 {
+    public static void name() {
+        System.out.println("Bienvenidos a GitHub");
+        
+    }
+    
+}
