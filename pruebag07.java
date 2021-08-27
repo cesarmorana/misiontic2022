5e5c93c6750be93a6a4a69b9b94111d6c56afd3a
andrmoran <89643687+andrmoran@users.noreply.github.com>
2021-08-27 07:50:57 -0500

    version1.1
    versión 1.1

--- pruebag07.java
+++ pruebag07.java
@@ -2,7 +2,7 @@ import javax.swing.plaf.synth.SynthScrollableTabButton;
 
 public class pruebag07 {
     public static void name() {
-        System.out.println("Bienvenidos a GitHub");
+        System.out.println("Bienvenidos a GitHub MisionTIC2022 G7");
         
     }
     
